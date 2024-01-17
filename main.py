aee36b137991a1b4cf7b82e23c91450d955a73bb
alex <92396340+helloalicealexp@users.noreply.github.com>
2024-01-16 19:01:18 -0800

    it works!

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
 import sys
+import json
 from lxml import etree
+import os
 
 
 def generate_unique_xpaths(xml_file_path, element_filter=None):
     tree = etree.parse(xml_file_path)
     root = tree.getroot()
-
-    def create_basic_xpath(element):
-        if element.get('name'):
-            return f"//{element.tag}[@name='{element.get('name')}']"
-        elif element.text and element.text.strip():
-            return f"//{element.tag}[text()='{element.text.strip()}']"
-        else:
-            return f"//{element.tag}"
-
-    def create_detailed_xpath(element):
-        attributes = [f"@{k}='{v}'" for k, v in element.items()]
-        return f"//{element.tag}[{' and '.join(attributes)}]"
-
-    all_xpaths = {}
+    xpath_data = []
 
     for element in root.iter():
         if element_filter and element_filter.lower() not in element.tag.lower():
             continue
 
-        basic_xpath = create_basic_xpath(element)
-        count = all_xpaths.get(basic_xpath, 0)
+        element_data = {
+            "name": element.tag,
+            "SimpleName": element.get('name', element.tag),
+            "XPath": generate_xpath(element)
+        }
+        xpath_data.append(element_data)
 
-        # Use basic xpath if unique, else create detailed xpath
-        xpath = basic_xpath if count == 0 else create_detailed_xpath(element)
+    return xpath_data
 
-        all_xpaths[basic_xpath] = count + 1
-        if count > 0:
-            xpath += f"[{count + 1}]"
 
-        yield xpath
+def generate_xpath(element):
+    if element.get('name'):
+        return f"//{element.tag}[@name='{element.get('name')}']"
+    elif element.text and element.text.strip():
+        return f"//{element.tag}[text()='{element.text.strip()}']"
+    else:
+        return f"//{element.tag}"
+
+
+def write_to_json_file(xpath_data, output_file):
+    output = {data["name"]: {
+        "SimpleName": data["SimpleName"],
+        "locators": {
+            "Roku": {
+                "AllLocales": {
+                    "AllDevices": f"XPath::{data['XPath']}"
+                }
+            }
+        }
+    } for data in xpath_data}
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        json.dump(output, file, indent=4)
 
 
 def main(xml_file_path, element_filter):
-    xpaths = list(generate_unique_xpaths(xml_file_path, element_filter))
-    for xpath in xpaths:
-        print(xpath)
+    xpath_data = generate_unique_xpaths(xml_file_path, element_filter)
+    output_file = os.path.join(os.path.dirname(xml_file_path), 'xpath_output.json')
+    write_to_json_file(xpath_data, output_file)
+    print(f"XPaths have been written to {output_file}")
 
 
 if __name__ == "__main__":
